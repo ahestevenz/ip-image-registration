731063994858f76f78584efcb60925190528ad73
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2015-11-12 09:46:25 -0300

    Modificado: [getMIRegistration.m] Se optimizó el relleno de los valores minimos en la imagen registrada.

--- getMIRegistration.m
+++ getMIRegistration.m
@@ -10,10 +10,7 @@ h=zeros(mv_rows,mv_cols,angle_cols);
 im1=image_1;
 im2=image_2;
 
-%%GPU Implementation
-%im1=gpuArray(image_1); 
-%im2=gpuArray(image_2);
-
+%Get the mutual information matrix
 parfor k=1:angle_cols
     im2_rot = imrotate(im2, angle(k),'bilinear','crop'); % Rotate and crop IMAGE2 
     h(:,:,k)=getMIMatrix(im1,im2_rot, mv_rows, mv_cols, step);
@@ -23,19 +20,10 @@ end
 [index_dy,index_dx,angle_index] = ind2sub(size(h),h_index);
 theta=angle(angle_index);
 
-%Rotate and translate with the MI maximal value
+% Rotate and translate with the MI maximal value
 image_rot = imrotate(image_2,theta,'bilinear','crop');
-
-%TODO: Sacar este código y hacerlo mejor.
-for i=1:rows
-    for j=1:cols
-        if (image_rot(i,j)<min(image_2(:)))
-        image_rot(i,j)=min(image_2(:));            
-        end        
-    end
-end
-%%%
-
+idx1 = (image_rot<min(image_2(:)));
+image_rot(idx1)=min(image_2(:));
 dx=round(index_dx-mv_cols/2);
 dy=round(index_dy-mv_rows/2);
 image_Reg = imtranslate(image_rot,[dx, dy],'FillValues',min(image_2(:)));
