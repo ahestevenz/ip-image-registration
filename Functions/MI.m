c901c10663e1b9fb5198f75368d639f33fd2883f
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2015-11-12 09:40:38 -0300

    Modificado: [MI.m] Se cambió el nombre y se modificó el código para que calcule la mutual information normalizada y estandar.

--- Functions/MI.m
+++ Functions/MI.m
@@ -1,18 +1,21 @@
-function M = MI_GG(X,Y)
-% function M = MI_GG(X,Y)
+function M = MI(X,Y,type)
+% function M = MI_GG(X,Y,type)
 % Compute the mutual information of two images: X and Y, having
 % integer values.
 % 
 % INPUT:
-% X --> first image 
-% Y --> second image (same size of X)
+% X     --> first image 
+% Y     --> second image (same size of X)
+% type  --> 'n' (normalized) or 's' (standart)
 %
 % OUTPUT:
 % M --> mutual information of X and Y
 %
 % Written by GIANGREGORIO Generoso. 
+% Modified by HERNANDEZ Ariel
 % DATE: 04/05/2012
 % E-MAIL: ggiangre@unisannio.it
+% E-MAIL: ahernandez@cae.cnea.gov.ar
 %__________________________________________________________________________
 
 X = double(X);
@@ -33,6 +36,20 @@ Hy = - sum(y_marg.*log2(y_marg + (y_marg == 0))); % Entropy of Y
 Hx = - sum(x_marg.*log2(x_marg + (x_marg == 0))); % Entropy of X
 
 arg_xy2 = hn.*(log2(hn + (hn==0)));
-h_xy = sum(-arg_xy2(:)); % joint entropy
-M = Hx + Hy - h_xy; % mutual information
+Hxy = sum(-arg_xy2(:)); % joint entropy
+
+switch type
+    case 'n'
+        M = (Hx + Hy)/Hxy; % normalized mutual information 
+    case 's'
+        M = Hx + Hy - Hxy; % standart mutual information
+    otherwise
+        disp('Wrong method! Available methods: ')
+        disp('* "n": normalized')
+        disp('* "s": standart')
+        M=NaN;
+        return
+end
+
+end
 
