2d75a01ffe86144dd9b5ac630adeafe4ae1d0159
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2015-11-12 15:59:08 -0300

    Agregado: [MetropolisMIRegister.m] Se agregaron los gráficos de los parámetros estimados y aceptados. Se acomodó el script para usar la función de registración con Metropolis.

--- MetropolisMIRegister.m
+++ MetropolisMIRegister.m
@@ -10,14 +10,9 @@
 close all
 clear
 
-%% Parameters
-angle=-45:5:45;
-step=200;
-
 %% Getting DICOM images
-patient=007;
+patient=006;
 [moving_16bit,fixed_16bit]=dicomOpen(patient);
-%moving_16bit=imresize(moving_original_16bit,4); % Resize moving image (http://www.mathworks.com/help/images/examples/registering-multimodal-3-d-medical-images.html)
 
 %% Convert to 8bit
 moving_8bit=im2uint8(moving_16bit);
@@ -25,29 +20,65 @@ fixed_8bit=im2uint8(fixed_16bit);
 
 %% MATLAB Image Registration (IP Toolbox)
 % [movingReg_16bit, optimizer, metric] = imgRegister(moving_16bit, fixed_16bit, -1, -1, -1, -1, -1, -1, -1, 'affine');
-% figure('Name',['Patient ' num2str(patient) ': Default registration on affine transformation model (16bit)']);
-% imshowpair(movingReg_16bit, fixed_16bit);
 % movingReg_8bit=im2uint8(movingReg_16bit);
-% disp('1: Default registration on affine transformation model')
 % disp(optimizer)
 % disp(metric)
 
 %% Image Registration Mutual Information
-%[h, h_max_value,movingMIReg, theta,dx,dy] = getMIRegistration(fixed_8bit,moving_8bit,angle,step);
 [ movingMIReg, P, mi, th_vec, tx_vec, ty_vec, MI_vec, th_vec_a, tx_vec_a, ty_vec_a, MI_vec_a] = getMetropolisMIRegistration(fixed_8bit, moving_8bit);
 
+%% Metrics: Graphics
 
-%% Graphics
-figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
-imshowpair(moving_16bit,fixed_16bit);
-figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
-imshowpair(moving_8bit,fixed_8bit);
-figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
-imshowpair(movingReg_8bit,fixed_8bit);
+% Accepted
+figure('Name',['Patient ' num2str(patient) ': Parameters accepted']);
+subplot(2,2,1)
+plot(th_vec_a)
+title('Angle of Rotation')
+
+subplot(2,2,2)
+plot(tx_vec_a)
+title('Translation in X axis')
+
+subplot(2,2,3)
+plot(ty_vec_a)
+title('Translation in Y axis')
+
+subplot(2,2,4)
+plot(MI_vec_a)
+title('Mutual Information')
+
+% All 
+figure('Name',['Patient ' num2str(patient) ': All parameters']);
+subplot(2,2,1)
+plot(th_vec)
+title('Angle of Rotation')
+
+subplot(2,2,2)
+plot(tx_vec)
+title('Translation in X axis')
+
+subplot(2,2,3)
+plot(ty_vec)
+title('Translation in Y axis')
+
+subplot(2,2,4)
+plot(MI_vec)
+title('Mutual Information')
+
+%% Registration: Graphics
+% figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
+% imshowpair(moving_16bit,fixed_16bit);
+% figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
+% imshowpair(moving_8bit,fixed_8bit);
+% figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
+% imshowpair(movingReg_8bit,fixed_8bit);
 figure('Name',['Patient ' num2str(patient) ': Registered images with Mutual Information (8bit)']);
 imshowpair(movingMIReg,fixed_8bit);
-figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
-imshowpair(movingMIReg,movingReg_8bit);
+% figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
+% imshowpair(movingMIReg,movingReg_8bit);
+
+%% Save all matrices
+save(['Output/image_registration_patient_' num2str(patient) '_mi_metropolis_algorithm']);
 
 
 
