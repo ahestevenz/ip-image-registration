6550d1bdb25f95319125e0a65601ba1396d145b7
Ariel Hernandez <ahestevenz@Kepler.proto>
2015-11-12 11:21:40 -0300

    Modificado: [MIRegister.m] Se acomodó el código para procesar los datos de salida.

--- MIRegister.m
+++ MIRegister.m
@@ -11,11 +11,12 @@ close all
 clear
 
 %% Parameters
-angle=-45:1:45;
-step=1;
+step_angle=0.5;
+angle=-45:step_angle:45;
+step_pixel=1;
 
 %% Getting DICOM images
-patient=007;
+patient=002;
 [moving_16bit,fixed_16bit]=dicomOpen(patient);
 
 %% Convert to 8bit
@@ -24,28 +25,29 @@ fixed_8bit=im2uint8(fixed_16bit);
 
 %% MATLAB Image Registration (IP Toolbox)
 [movingReg_16bit, optimizer, metric] = imgRegister(moving_16bit, fixed_16bit, -1, -1, -1, -1, -1, -1, -1, 'affine');
-figure('Name',['Patient ' num2str(patient) ': Default registration on affine transformation model (16bit)']);
-imshowpair(movingReg_16bit, fixed_16bit);
 movingReg_8bit=im2uint8(movingReg_16bit);
-disp('1: Default registration on affine transformation model')
 disp(optimizer)
 disp(metric)
 
 %% Image Registration Mutual Information
-[h, h_max_value,movingMIReg, theta,dx,dy] = getMIRegistration(fixed_8bit,moving_8bit,angle,step);
+[h, h_max_value,movingMIReg, theta,dx,dy] = getMIRegistration(fixed_8bit,moving_8bit,angle,step_pixel);
 
 
 %% Graphics
-figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
-imshowpair(moving_16bit,fixed_16bit);
-figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
-imshowpair(moving_8bit,fixed_8bit);
-figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
-imshowpair(movingReg_8bit,fixed_8bit);
-figure('Name',['Patient ' num2str(patient) ': Registered images with Mutual Information (8bit)']);
-imshowpair(movingMIReg,fixed_8bit);
-figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
-imshowpair(movingMIReg,movingReg_8bit);
+%figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
+%imshowpair(moving_16bit,fixed_16bit);
+%figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
+%imshowpair(moving_8bit,fixed_8bit);
+%figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
+%imshowpair(movingReg_8bit,fixed_8bit);
+%figure('Name',['Patient ' num2str(patient) ': Registered images with Mutual Information (8bit)']);
+%imshowpair(movingMIReg,fixed_8bit);
+%figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
+%imshowpair(movingMIReg,movingReg_8bit);
+
+%% Save all matrices
+save(['image_registration_patient_' num2str(patient) '_step_pixel_' num2str(step_pixel) '_step_angle_' num2str(step_angle) '_mi_get_matrix']);
+
 
 
 
