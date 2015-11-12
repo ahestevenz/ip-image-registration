96bd358b816800371f5ec8652b12b3dd86fc1ff1
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2015-11-12 09:47:14 -0300

    Modificado: [getMIMatrix.m] Se modificó la llamada a la función MI.m

--- getMIMatrix.m
+++ getMIMatrix.m
@@ -6,7 +6,7 @@ hij=zeros(mv_rows,mv_cols);
 for i=1:step:mv_rows
     for j=1:step:mv_cols
         im2=imtranslate(image_2,[round(j-mv_cols/2),round(i-mv_rows/2)]);         
-        hij(i,j)=MI_GG(im2,im1);
+        hij(i,j)=MI(im2,im1,'s');
     end
 end
 
