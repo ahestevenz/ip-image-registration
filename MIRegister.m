d64299cbb21cfff4d6a04ef0d39d7ff6016d1084
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2015-11-12 11:38:33 -0300

    Modificado: [MIRegister.m] Se dejó para que se realice la registración a partir del cálculo de la matriz de mutual information.

--- MIRegister.m
+++ MIRegister.m
@@ -11,42 +11,41 @@ close all
 clear
 
 %% Parameters
-angle=-45:5:45;
-step=200;
+angle=-45:1:45;
+step=1;
 
 %% Getting DICOM images
 patient=007;
 [moving_16bit,fixed_16bit]=dicomOpen(patient);
-%moving_16bit=imresize(moving_original_16bit,4); % Resize moving image (http://www.mathworks.com/help/images/examples/registering-multimodal-3-d-medical-images.html)
 
 %% Convert to 8bit
 moving_8bit=im2uint8(moving_16bit);
 fixed_8bit=im2uint8(fixed_16bit);
 
 %% MATLAB Image Registration (IP Toolbox)
-% [movingReg_16bit, optimizer, metric] = imgRegister(moving_16bit, fixed_16bit, -1, -1, -1, -1, -1, -1, -1, 'affine');
-% figure('Name',['Patient ' num2str(patient) ': Default registration on affine transformation model (16bit)']);
-% imshowpair(movingReg_16bit, fixed_16bit);
-% movingReg_8bit=im2uint8(movingReg_16bit);
-% disp('1: Default registration on affine transformation model')
-% disp(optimizer)
-% disp(metric)
+[movingReg_16bit, optimizer, metric] = imgRegister(moving_16bit, fixed_16bit, -1, -1, -1, -1, -1, -1, -1, 'affine');
+figure('Name',['Patient ' num2str(patient) ': Default registration on affine transformation model (16bit)']);
+imshowpair(movingReg_16bit, fixed_16bit);
+movingReg_8bit=im2uint8(movingReg_16bit);
+disp('1: Default registration on affine transformation model')
+disp(optimizer)
+disp(metric)
 
 %% Image Registration Mutual Information
 [h, h_max_value,movingMIReg, theta,dx,dy] = getMIRegistration(fixed_8bit,moving_8bit,angle,step);
 
 
 %% Graphics
- figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
- imshowpair(moving_16bit,fixed_16bit);
-% figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
-% imshowpair(moving_8bit,fixed_8bit);
-% figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
-% imshowpair(movingReg_8bit,fixed_8bit);
-% figure('Name',['Patient ' num2str(patient) ': Registered images with Mutual Information (8bit)']);
-% imshowpair(movingMIReg,fixed_8bit);
-% figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
-% imshowpair(movingMIReg,movingReg_8bit);
+figure('Name',['Patient ' num2str(patient) ': Unregistered images (16bit)']);
+imshowpair(moving_16bit,fixed_16bit);
+figure('Name',['Patient ' num2str(patient) ': Unregistered images (8bit)']);
+imshowpair(moving_8bit,fixed_8bit);
+figure('Name',['Patient ' num2str(patient) ': Registered images with IP Toolbox (8bit)']);
+imshowpair(movingReg_8bit,fixed_8bit);
+figure('Name',['Patient ' num2str(patient) ': Registered images with Mutual Information (8bit)']);
+imshowpair(movingMIReg,fixed_8bit);
+figure('Name',['Patient ' num2str(patient) ': Comparison between two registrations (8bit)']);
+imshowpair(movingMIReg,movingReg_8bit);
 
 
 
