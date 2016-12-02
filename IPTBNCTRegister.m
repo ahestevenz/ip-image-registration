4499f89176f5823f9f3fddc54dccd8f8fd60a870
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-12-02 15:33:07 -0300

    Modificado: [IPTBNCTRegister.m] Estructura del script.
    Se realizaron pruebas con las imágenes radiográficas sin problemas. Las modificaciones agregadas son de forma, que la salida de la matriz de transformación sea clara.

--- IPTBNCTRegister.m
+++ IPTBNCTRegister.m
@@ -11,20 +11,33 @@ addpath(genpath('functions/'))
 
 %% Obtengo las imágenes BNCT
 [moving, fixed]=imagesBNCTPairOpen(strcat(absolute_principal_path,'/',sample_directory));
-movig_mod=imrotate(moving,1);
+
 % Gráficos
 figure('Name', [ sample_directory, ' : Unregistered images']);
-imshowpair(moving,fixed, 'montage');
+imshowpair(fixed, moving, 'montage');
 
 %% Registración
+disp('** Default registration on rigid transformation model')
 %[movingReg, optimizer, metric, tformSimilarity] = imgTFormRegister(moving, fixed, growthfactor, epsilon, initialradius, iterations, samples, histogrambins, pixels, type)
-[movingReg, optimizer, metric, tformSimilarity] = imgTFormRegister(moving, fixed, -1, -1, -1, -1, -1, -1, -1, 'rigid');
+[movingReg, optimizer, metric, tformRigid] = imgTFormRegister(moving, fixed, -1, -1, -1, -1, -1, -1, -1, 'rigid');
 figure('Name',[ sample_directory, ' : Default registration on rigid transformation model']);
 imshowpair(movingReg, fixed);
-disp('Default registration on rigid transformation model')
 disp(optimizer)
 disp(metric)
-tformSimilarity.T
+
+%% Datos de la registración
+disp('** Transformation matrix parameters')
+trasformation_matrix=tformRigid.T;
+disp(trasformation_matrix)
+angle=['Angulo de rotación: ',num2str(acos(trasformation_matrix(1)))];
+dx=['Desplazamiento en X: ',num2str(trasformation_matrix(3))];
+dy=['Desplazamiento en Y: ',num2str(trasformation_matrix(6))];
+disp(angle)
+disp(dx)
+disp(dy)
+
+
+
 
 
 
